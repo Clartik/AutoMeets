2a55b687299643a311411302a99efeecda576f53
Clartik <yg.kartik@gmail.com>
2022-09-11 01:54:25 -0700

    Uploading Files to GitHub

--- AutoMeetsExtension/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  chrome.tabs.query({active: true}, function(tabs){
-    if(tabs[0].url === "https://sites.google.com/view/automeets-launch-site/home") {
-      chrome.tabs.remove(tabs[0].id);
-    }
-  });
-  chrome.tabs.create({url: "https://www.google.com"});
-  chrome.tabs.sendMessage(0, "Test1");
-});
