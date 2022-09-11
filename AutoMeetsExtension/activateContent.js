60e4177ffee0af766ab5673291ebc8f4c97b28db
Clartik <yg.kartik@gmail.com>
2022-09-11 01:56:04 -0700

    Uploading Files to GitHub

--- AutoMeetsExtension/activateContent.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.sendMessage("Test");
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    var yes = document.getElementsByClassName("gb_Oa gb_dd gb_xg gb_i gb_Mf gb_ma");
-    console.log(yes);
-});
